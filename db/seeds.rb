7821bbef3c7e60eae7c80a2e80871a36d857df2e
Mateus Amaral <mateuspa@gmail.com>
2018-01-26 17:56:55 -0200

    adicionada funcionalidades no controller offers, exibicao das offers na view e alguns exemplos para testes no seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Offer.destroy_all
+Offer.new (:title "Cebolinha", :description "Da hora essa cebolinha", :photo "", :address "Sion", :available "true", :price "2,40", :seller_id "1")
+Offer.new (:title "Cerveja", :description "IPA do Cajuru", :photo "", :address "Alto Barroca", :available "true", :price "22", :seller_id "1")
+Offer.new (:title "Bolo", :description "Trufado com granulados", :photo "", :address "Gutierrez", :available "true", :price "15,80", :seller_id "2")
+Offer.new (:title "Iogurte", :description "", :photo "", :address "Pampulha", :available "true", :price "7,90", :seller_id "2")
+Offer.new (:title "Mel", :description "", :photo "", :address "Carmo", :available "true", :price "8", :seller_id "3")
