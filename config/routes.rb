48f98c07e018f0f5b217efdd79b55d77336d0982
Mateus Amaral <mateuspa@gmail.com>
2018-01-26 14:53:30 -0200

    User and Offer routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,8 @@
 Rails.application.routes.draw do
-  get 'offers/index'
-
-  get 'offers/show'
-
-  get 'offers/new'
-
-  get 'offers/create'
-
-  get 'offers/edit'
-
-  get 'offers/update'
-
-  get 'offers/destroy'
-
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/edit'
-
-  get 'users/update'
-
   devise_for :users
   root to: 'pages#home'
+
+  resources :users
+  resources :offer
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
