6d4ebf8ab2c1b00c89eca236ae2845a994190d07
Mateus Amaral <mateuspa@gmail.com>
2018-01-26 16:17:25 -0200

    removendo a rota destroy do user e aninhando o offer em user

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :users
-  resources :offer
+  resources :users, except: [ :destroy ] do
+    resources :offer
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
