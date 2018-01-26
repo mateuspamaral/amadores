02295de814af80f171160518b079177351db5668
Mateus Amaral <mateuspa@gmail.com>
2018-01-26 18:36:13 -0200

    seed finalmente certo

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Offer.destroy_all
-Offer.new (title: "Cebolinha", description: "Da hora essa cebolinha", photo: "", address: "Sion", available: "true", price: "2,40", seller_id: "1")
-Offer.new (title: "Cerveja", description: "IPA do Cajuru", photo: "", address: "Alto Barroca", available: "true", price: "22", seller_id: "1")
-Offer.new (title: "Bolo", description: "Trufado com granulados", photo: "", address: "Gutierrez", available: "true", price: "15,80", seller_id: "2")
-Offer.new (title: "Iogurte", description: "", photo: "", :address "Pampulha", available: "true", price: "7,90", seller_id: "2")
-Offer.new (title: "Mel", description: "", photo: "", :address "Carmo", available: "true", price: "8", seller_id: "3")
+Offer.create(title: "Cebolinha", description: "Da hora essa cebolinha", photo: "", address: "Sion", available: "true", price: 2.40, seller_id: 1)
+Offer.create(title: "Cerveja", description: "IPA do Cajuru", photo: "", address: "Alto Barroca", available: "true", price: 22, seller_id: 1)
+Offer.create(title: "Bolo", description: "Trufado com granulados", photo: "", address: "Gutierrez", available: "true", price: 15.80, seller_id: 2)
+Offer.create(title: "Iogurte", description: "Pra te deixar doidao", photo: "", :address "Pampulha", available: "true", price: 7.90, seller_id: 2)
+Offer.create(title: "Mel", description: "Da loucura", photo: "", :address "Carmo", available: "true", price: 8, seller_id: 3)
