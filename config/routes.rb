e50d6f09c5866b7e3e5f04a267b55e0b1503bba3
Mateus Amaral <mateuspa@gmail.com>
2018-01-29 11:42:45 -0200

    alterada as rotas

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :users, except: [ :destroy ] do
-    resources :offer
-  end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :offers
 end
