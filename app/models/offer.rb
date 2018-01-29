c3d8006445a3a563b0e9282419989d0ca1252be0
Mateus Amaral <mateuspa@gmail.com>
2018-01-29 21:56:13 -0200

    #26

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,10 @@
 class Offer < ApplicationRecord
+  #add photo upload function
   mount_uploader :photo, PhotoUploader
+
+  #References
   belongs_to :user
+
+  #Validations
+  validates :title, :photo, :address, presence: true
 end
